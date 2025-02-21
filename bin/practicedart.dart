9109101b9d54e7dd0f7dc453dafeb64df30114ad
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-21 09:43:21 +0600

    innit commit

--- bin/practicedart.dart
+++ bin/practicedart.dart
@@ -1,6 +1,7 @@
 
  void main(){
   List<int> numberList=[1,2,3,4,5,6];
- var reverseList=numberList.reversed.toList();
-  print(reverseList);
+ 
+  int maxnumber=numberList.reduce((a,b)=>a>b?a:b);
+  print(maxnumber);
 }
