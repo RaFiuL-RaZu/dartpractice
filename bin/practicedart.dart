3e496a2657cd6c2ef6c85cba9dc1d58b860abd77
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-21 10:13:38 +0600

    innit commit

--- bin/practicedart.dart
+++ bin/practicedart.dart
@@ -1,6 +1,6 @@
 
  void main(){
-  List<String> nameList=["razu","sazu","rafi","safin"];
-
-  print(nameList.contains("tazu"));
+  List<int> nameList=[10,15,20,25,30,35,40];
+  var filterList=nameList.where((num)=> num > 25).toList();
+  print(filterList);
 }
