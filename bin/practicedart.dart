226dc2673063f6bfd37615569fd419250f363190
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-21 09:44:20 +0600

    innit commit

--- bin/practicedart.dart
+++ bin/practicedart.dart
@@ -3,5 +3,7 @@
   List<int> numberList=[1,2,3,4,5,6];
  
   int maxnumber=numberList.reduce((a,b)=>a>b?a:b);
+  int minnumber=numberList.reduce((a,b)=> a<b?a:b);
   print(maxnumber);
+  print(minnumber);
 }
