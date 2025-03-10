5890528755d0f802ada51dca07c9f115d9a29639
RaFiuL-RaZu <rafi102621@gmail.com>
2025-03-11 02:50:26 +0600

    duplicate method commit

--- bin/practicedart.dart
+++ bin/practicedart.dart
@@ -2,7 +2,7 @@
  import 'dart:math';
 
  void main() {
-   List<int> num=[7,2,9,4,1,6,3,5];
-   num.sort();
-   print(num);
+   List<int> num=[7,2,9,4,1,6,3,5,5,7,3];
+  var duplicate= num.toSet().toList();
+   print(duplicate);
  }
