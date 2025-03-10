a6bf029458f581f0ba3a1b95d19e7f6ed5614926
RaFiuL-RaZu <rafi102621@gmail.com>
2025-03-11 01:59:27 +0600

    bool commit

--- bin/practicedart.dart
+++ bin/practicedart.dart
@@ -3,6 +3,12 @@
 
 void main(){
   List<int> numbers= [1,2,3,4,5,6];
-bool result= numbers.isNotEmpty;
-  print(result);
+  bool result=numbers.isEmpty;
+  if(result==true){
+    print("correct");
+  }
+  else{
+    print("Incorrect");
+  }
+
 }
